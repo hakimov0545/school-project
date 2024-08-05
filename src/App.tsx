edea73d45e3e20d48317bcf48631dd42aa8f2cc2
hakimov0545 <abdulazizhakimov13@gmail.com>
2024-08-05 12:33:29 +0500

    hello world

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,11 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div>sdasdas</div>;
+	return (
+		<div>
+			<h2>Hello world</h2>
+		</div>
+	);
 }
 
 export default App;
