8525445738f292623caf9dbb4f8db6d54bc68ac9
Vasko038 <Vasko038@outlook.com>
2024-08-06 18:13:55 +0500

    sdasd

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,11 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-	return (
-		<div>
-			<h2>Hello world</h2>
-		</div>
-	);
+  return (
+    <div>
+      <h2>Hello world</h2>
+    </div>
+  );
 }
 
 export default App;
