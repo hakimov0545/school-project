0eba4a048801db47968c44a5648f91f8b0b8d0b8
Vasko038 <Vasko038@outlook.com>
2024-08-07 08:53:28 +0500

    salom

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React, { createContext, useContext, useState } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import TeacherPanel from "./Pages/TeacherPanel";
 import { IClass, IStudent, ITeacher } from "./Types";
